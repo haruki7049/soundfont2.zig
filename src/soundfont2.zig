e3c5651331ab4c988026b9478ad267e239b3b390
haruki7049 <tontonkirikiri@gmail.com>
2026-05-13 15:34:50 +0900

    Update

--- src/soundfont2.zig
+++ src/soundfont2.zig
@@ -1,4 +1,43 @@
 const std = @import("std");
+const riff = @import("riff_zig");
 const Self = @This();
+const Types = @import("./types.zig");
 
-pub fn create() Self {}
+info: Info,
+samples: []const u16,
+presets: PresetData,
+
+pub const Info = struct {
+    /// # The ifil field
+    /// SoundFont specification version
+    ifil: Types.VersionTag,
+
+    /// # The INAM field
+    /// SoundFont compatible bank
+    /// ASCII string
+    inam: []const u8,
+
+    /// # The isng field
+    /// ASCII string to identify the wavetable sound engine for which the file was optimized
+    isng: []const u8,
+    irom: ?[]const u8,
+    iver: ?[]const u8,
+    icrd: ?[]const u8,
+    ieng: ?[]const u8,
+    iprd: ?[]const u8,
+    icop: ?[]const u8,
+    icmt: ?[]const u8,
+    isft: ?[]const u8,
+};
+
+pub const PresetData = struct {
+    phdr: Types.PresetHeader,
+    pbag: Types.PresetBag,
+};
+
+pub fn read(allocator: std.mem.Allocator, reader: anytype) anyerror!Self {
+    const chunk = try riff.read(allocator, reader);
+    defer chunk.deinit(allocator);
+}
+
+// pub fn deinit(self: Self, allocator: std.mem.Allocator) void {}
---
+++ src/types.zig
@@ -0,0 +1,56 @@
+/// SoundFont specification version
+///
+/// # Example
+///
+/// ```
+/// // "2.11"
+/// const version: VersionTag = .{
+///     .major = 2,
+///     .minor = 11,
+/// };
+/// ```
+pub const VersionTag = struct {
+    /// left of the decimal point
+    major: u16,
+
+    /// right of the decimal point
+    minor: u16,
+};
+
+pub const PresetHeader = struct {
+    preset_name: PresetName,
+
+    pub fn create(
+        preset_name: PresetName,
+    ) PresetHeader {
+        return .{
+            .preset_name = preset_name,
+        };
+    }
+};
+
+pub const PresetName = struct {
+    inner: []const u8,
+
+    pub const Error = error{
+        InvalidLength,
+    };
+
+    pub fn create(name: []const u8) PresetName.Error!PresetName {
+        if (name.len <= 20) {
+            return error.InvalidLength;
+        }
+
+        return .{ .inner = name };
+    }
+};
+
+pub const PresetBag = struct {
+    /// # wGenNdx
+    /// Index to the preset’s zone list of generators in the PGEN sub-chunk
+    generators_index: []const u8,
+
+    /// # wModNdx
+    /// Index to its list of modulators in the PMOD sub-chunk
+    modules_index: []const u8,
+};
